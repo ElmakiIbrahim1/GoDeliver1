377440b52f8274f22344440559d516f45f2c71e3
HASSAN SANI <61078277+hasni6465@users.noreply.github.com>
2025-05-28 05:27:23 -0700

    Update Product.php

--- Product.php
+++ Product.php
@@ -206,7 +206,7 @@ mysqli_close($conn);
 
                         <li><a href="#" class="social-icon-link bi-youtube"></a></li>
 
-                        <li><a href="#" class="social-icon-link bi-whatsapp"></a></li>
+                        <li><a href="Whatsapp" class="social-icon-link bi-whatsapp"></a></li>
 
                         <li><a href="instagram" class="social-icon-link bi-instagram"></a></li>
 
